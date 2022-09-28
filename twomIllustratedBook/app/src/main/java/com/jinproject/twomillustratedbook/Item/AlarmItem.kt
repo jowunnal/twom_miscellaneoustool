c55925a4e4f18b21667a73ee647615ce3a7ff540
jino <whangwan@gmail.com>
2022-09-28 16:11:40 +0900

    Auto stash before merge of "master" and "origin/master"
    파라미터가 없는 생성자 추가

--- twomIllustratedBook/app/src/main/java/com/jinproject/twomillustratedbook/Item/AlarmItem.kt
+++ twomIllustratedBook/app/src/main/java/com/jinproject/twomillustratedbook/Item/AlarmItem.kt
@@ -3,4 +3,5 @@ package com.jinproject.twomillustratedbook.Item
 import java.util.*
 
 data class AlarmItem(val name:String,val imgName:String,val code:Int,val gtime:Int){
+    constructor():this("","",0,0)
 }
