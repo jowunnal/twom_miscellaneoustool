c1139052534ba89c3aaaad9b4d95bb5556d391d2
jino <whangwan@gmail.com>
2022-10-06 20:53:43 +0900

    User 릴레이션 및 클래스 제거

--- twomIllustratedBook/app/src/main/java/com/jinproject/twomillustratedbook/Item/User.kt
+++
@@ -1,9 +0,0 @@
-package com.jinproject.twomillustratedbook.Item
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-data class User(var userName:String,@PrimaryKey  var userId:String, var userPw:String, var userAuthority:String){
-    constructor():this("","","","")
-}
