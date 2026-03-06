402f33768e883b6d168681dbd5d4475612dbf440
jinho <whangwan@gmail.com>
2026-03-07 00:43:28 +0900

    fix: 광고 정책 위반 수정
    - 리워드 광고의 경우 사용자가 광고를 볼지, 말지 결정할 수 있는 UX 를 제공해야 함. 따라서, 버튼에 광고가 나올 수 있음을 표시하도록 수정

--- design/src/main/kotlin/com/jinproject/design_compose/component/button/TextButton.kt
+++ design/src/main/kotlin/com/jinproject/design_compose/component/button/TextButton.kt
@@ -1,15 +1,24 @@
 package com.jinproject.design_compose.component.button
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jinproject.design_compose.component.HorizontalSpacer
 import com.jinproject.design_compose.utils.PreviewMiscellaneousToolTheme
 import com.jinproject.design_compose.theme.Typography
+import com.jinproject.design_ui.R
 
 @Composable
 fun TextButton(
@@ -37,6 +46,38 @@ fun TextButton(
 }
 
 @Composable
+fun TextIconButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    contentPaddingValues: PaddingValues = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+    shape: RoundedCornerShape = RoundedCornerShape(100.dp),
+    enabled: Boolean = true,
+    icon: @Composable RowScope.() -> Unit,
+    onClick: () -> Unit,
+) {
+    DefaultButton(
+        modifier = modifier,
+        backgroundColor = MaterialTheme.colorScheme.primary,
+        contentPaddingValues = contentPaddingValues,
+        shape = shape,
+        onClick = onClick,
+        enabled = enabled,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            icon()
+            HorizontalSpacer(4.dp)
+            Text(
+                text = text,
+                style = Typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onPrimary,
+            )
+        }
+    }
+}
+
+@Composable
 fun TextCombinedButton(
     text: String,
     modifier: Modifier = Modifier,
@@ -60,7 +101,7 @@ fun TextCombinedButton(
     }
 }
 
-@Preview()
+@Preview
 @Composable
 private fun PreviewDefaultButton() =
     PreviewMiscellaneousToolTheme {
@@ -70,7 +111,24 @@ private fun PreviewDefaultButton() =
         )
     }
 
-@Preview()
+@Preview
+@Composable
+private fun PreviewTextIconButton() =
+    PreviewMiscellaneousToolTheme {
+        TextIconButton(
+            text = "버튼",
+            icon = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_play_rounded),
+                    contentDescription = "Play Ad",
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                )
+            },
+            onClick = {}
+        )
+    }
+
+@Preview
 @Composable
 private fun PreviewSelectionButton() =
     PreviewMiscellaneousToolTheme {
